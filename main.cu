031d1bba7ec3fe16e4be74ae59107aa4e2631f97
Gus <floresgalindog@gmail.com>
2026-05-20 13:08:07 -0700

    códigos requeridos de stb_image ystb_image_write

--- main.cu
+++ main.cu
@@ -13,6 +13,12 @@
 #include <string>
 #include <algorithm>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 #define CUDA_CHECK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
